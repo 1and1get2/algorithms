41e40fb634e2c9b6659acf6ff6d6d4b6eea97dc1
Derek Zhu <1and1get2@gmail.com>
2014-02-09 01:01:15 +1300

    first commit

---
+++ src/L06_quickFindUF/L06_QuickFindUF.java
@@ -0,0 +1,22 @@
+package L06_quickFindUF;
+
+public class L06_QuickFindUF {
+	private static int N = 25;
+	private int[] id;
+
+	public L06_QuickFindUF(int[] id) {
+		id = new int[N];
+		for (int i = 0; i < N; i++){
+			id[i] = i;
+		}
+	}
+	public boolean connected(int p, int q){
+		return id[p] == id[q];
+	}
+	
+	public void union(int p, int q){
+		for (int i = 0; i < N; i++){
+			if (id[i] == id[q]) id[i] = id[p];
+		}
+	}
+}
