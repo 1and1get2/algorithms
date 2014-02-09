138f5c8a661580a92c66c9b2daae8e763a1b57ff
Derek Zhu <1and1get2@gmail.com>
2014-02-09 14:22:08 +1300

    add weighted quich union

---
+++ src/w01_union_find/L08_QuickUnion_LazyApproach_weighted.java
@@ -0,0 +1,52 @@
+package w01_union_find;
+
+import java.util.Arrays;
+
+public class L08_QuickUnion_LazyApproach_weighted {
+	private static int N = 25;
+	private int numbers;
+	private int[] id;
+	
+	public L08_QuickUnion_LazyApproach_weighted(int[] id) {
+		if (id != null){
+			this.numbers = id.length;
+			this.id = id;
+		}
+	}
+	public L08_QuickUnion_LazyApproach_weighted() {
+		this(N);
+	}
+	public L08_QuickUnion_LazyApproach_weighted(int numbers) {
+		this.numbers = numbers;
+		id = new int[numbers];
+		for (int i = 0; i < numbers; i++){
+			id[i] = i;
+		}
+	}
+	
+	
+	// my own 
+	public int root(int p){
+		return id[p] == p ? p : root(id[p]);
+	}
+	
+	public void union(int p, int q){
+		id[root(p)] = root(q);
+	}
+	
+	public boolean connected(int p, int q){
+		return id[root(p)] == id[root(q)];
+	}
+	
+	public int getNumbers(){
+		return this.numbers;
+	}
+	
+	public int[] toArray(){
+		return id;
+	}
+	
+	public String toString(){
+		return Arrays.toString(id);
+	}
+}
