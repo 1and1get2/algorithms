54b5f07d9eca5733259f099d11af30059e52a763
Derek Zhu <1and1get2@gmail.com>
2014-02-19 18:57:50 +1300

    week 2 add capacity

--- src/w02_stacks_queues/L02_StackOfString_Array_FixedCapacity.java
+++ src/w02_stacks_queues/L02_StackOfString_Array_FixedCapacity.java
@@ -6,9 +6,9 @@ public class L02_StackOfString_Array_FixedCapacity implements L00_Stack_Queues{
 	private int N = 0;
 
 	
-	public L02_StackOfString_Array_FixedCapacity(int capacity) {
+	public L02_StackOfString_Array_FixedCapacity() {
 		super();
-		s = new String[capacity];
+		s = new String[1];
 	}
 
 	@Override
@@ -18,7 +18,17 @@ public class L02_StackOfString_Array_FixedCapacity implements L00_Stack_Queues{
 
 	@Override
 	public void push(String t) {
+		if (N == s.length) resize(2 * s.length); 
 		s[N++] = t;
+	}
+	
+	private void resize(int capacity){
+		String[] copy = new String[capacity];
+//		for (int i = 0; i < N; i++){
+//			copy[i] = s[i];
+//		}
+		System.arraycopy(s, 0, s, 0, N);
+		s = copy;
 	}
 
 	
@@ -32,6 +42,7 @@ public class L02_StackOfString_Array_FixedCapacity implements L00_Stack_Queues{
 	public String pop() {
 		String item = s[--N];
 		s[N] = null;
+		if (N > 0 && N == s.length / 4) resize(s.length/2);
 		return item;
 	}
 
---
+++ src/w02_stacks_queues/L02_StackOfString_Array_Resizing.java
@@ -0,0 +1,38 @@
+package w02_stacks_queues;
+
+public class L02_StackOfString_Array_Resizing implements L00_Stack_Queues{
+	
+	private String[] s;
+	private int N = 0;
+
+	
+	public L02_StackOfString_Array_Resizing(int capacity) {
+		super();
+		s = new String[capacity];
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return N == 0;
+	}
+
+	@Override
+	public void push(String t) {
+		s[N++] = t;
+	}
+
+	
+	@Override
+	/*
+	// loitering: hold a reference to an object when it is no longer needed
+	public String pop() {
+		return s[N--];
+	}
+	*/
+	public String pop() {
+		String item = s[--N];
+		s[N] = null;
+		return item;
+	}
+
+}
